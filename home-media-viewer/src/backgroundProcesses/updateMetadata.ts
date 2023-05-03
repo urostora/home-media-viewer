758a72a6ab0b4d4ccce31b60c8b7689a86e78454
urostora <urostora@gmail.com>
2023-05-03 20:24:27 +0200

    fix(background process): import resolution issue fixed

--- home-media-viewer/src/backgroundProcesses/updateMetadata.ts
+++ home-media-viewer/src/backgroundProcesses/updateMetadata.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { loadMetadata } from '../utils/fileHelper';
 import os from 'os';
+import { loadMetadata } from '@/utils/fileHelper';
+import { syncAlbumFiles } from '@/utils/albumHelper';
 
 const prisma = new PrismaClient();
 const threadCount = os.cpus().length;
@@ -14,18 +15,23 @@ const doJob = async () => {
   for (const album of activeAlbums) {
     console.log(`Processing album ${album.name} (${album.id})`);
 
-    const filesUnprocessed = await prisma.file.findMany({ where: { AND: [
-      { album, status: 'Active' },
-      { OR: [
-        { metadataStatus: 'New' },
-        { isDirectory: true } ]
-      }
-    ]}});
+    console.log('  Sync root files');
+    await syncAlbumFiles(album.id);
+
+    console.log('  Getting directories or files with unprocessed metadata');
+
+    const filesUnprocessed = await prisma.file.findMany({
+      where: { AND: [{ album, status: 'Active' }, { OR: [{ metadataStatus: 'New' }, { isDirectory: true }] }] },
+    });
     console.log(`  ${filesUnprocessed.length} unprocessed files found`);
 
     let fileIndex = 0;
     for (const file of filesUnprocessed) {
-      console.log(`  (${fileIndex}/${filesUnprocessed.length}) processing ${file.isDirectory ? 'directory' : 'file'} ${file.path} (${file.id})`);
+      console.log(
+        `  (${fileIndex}/${filesUnprocessed.length}) processing ${file.isDirectory ? 'directory' : 'file'} ${
+          file.path
+        } (${file.id})`,
+      );
 
       parallelJobs.push(loadMetadata(file, album));
 
