9e266ad302be3a21b35e2c2872796f46510a3637
attila bodi <urostora@gmail.com>
2023-12-06 17:13:21 +0100

    fix(page body): add proper pageBody style

--- home-media-viewer/src/components/layout/pageBody.tsx
+++ home-media-viewer/src/components/layout/pageBody.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react';
 
+import style from '@/styles/hmv.module.scss';
+
 const PageBody = (props: { children?: ReactNode }) => {
     return (
-        <div className="pageBody">
+        <div className={style.pageBody}>
             {props.children}
         </div>);
 }
