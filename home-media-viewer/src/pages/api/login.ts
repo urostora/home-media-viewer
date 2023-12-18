0e458cdb64ce3018c4e0635b6ad8ff01c5ce2645
attila bodi <urostora@gmail.com>
2023-12-18 13:55:53 +0100

    feat(api login): centralized error handling

--- home-media-viewer/src/pages/api/login.ts
+++ home-media-viewer/src/pages/api/login.ts
@@ -1,60 +1,64 @@
-import { type LoginRequestType, type LoginResponseDataType } from '@/types/loginTypes';
-import { getApiResponse, getApiResponseWithData, getRequestBodyObject } from '@/utils/apiHelpers';
-import { getIronSessionOptions } from '@/utils/sessionHelper';
-import { verifyPassword } from '@/utils/userHelper';
 import { withIronSessionApiRoute } from 'iron-session/next';
 
+import { HmvError, getApiResponseWithData, getRequestBodyObject, handleApiError } from '@/utils/apiHelpers';
+import { getIronSessionOptions } from '@/utils/sessionHelper';
+import { verifyPassword } from '@/utils/userHelper';
+
+import { type LoginRequestType, type LoginResponseDataType } from '@/types/loginTypes';
+import { type DataValidatorSchema, validateData } from '@/utils/dataValidator';
+
 import prisma from '@/utils/prisma/prismaImporter';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default withIronSessionApiRoute(async function loginRoute(req, res) {
-  const loginData: LoginRequestType | null = getRequestBodyObject(req, res);
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const schema: DataValidatorSchema = [
+    { field: 'email', isRequired: true },
+    { field: 'password', isRequired: true },
+  ];
 
-  if (loginData == null) {
-    req.session.destroy();
-    res.status(400).json(getApiResponse({ ok: false, error: 'Fields "email" and "password" must be set' }));
-    return;
+  try {
+    const loginData = getRequestBodyObject<LoginRequestType>(req, res);
+    validateData(loginData, schema);
+
+    const user = await prisma.user.findFirst({ where: { email: loginData.email } });
+
+    if (user == null) {
+      // unknown user
+      req.session.destroy();
+      throw new HmvError(`Login failed - wrong password for ${loginData.email} [IP: ${req.socket.remoteAddress}]`, {
+        publicMessage: 'Invalid email or password',
+      });
+    }
+
+    if (!(await verifyPassword(loginData.password, user.password))) {
+      // invalid password
+      req.session.destroy();
+      throw new HmvError(`Login failed - wrong password for ${user.email} [IP: ${req.socket.remoteAddress}]`, {
+        publicMessage: 'Invalid email or password',
+      });
+    }
+
+    const sessionOptions = getIronSessionOptions();
+
+    req.session.user = {
+      id: user?.id,
+      admin: user?.isAdmin ?? false,
+    };
+    await req.session.save();
+    console.log(`User ${user.name} logged in [IP: ${req.socket.remoteAddress}]`);
+
+    res.send(
+      getApiResponseWithData<LoginResponseDataType>({
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        sessionExpiresOn: new Date().getTime() + (sessionOptions?.cookieOptions?.maxAge ?? 24 * 60 * 60) * 1000,
+        sessionExpiresInSeconds: (sessionOptions?.cookieOptions?.maxAge ?? 24 * 60 * 60) * 1000,
+      }),
+    );
+  } catch (e) {
+    handleApiError(res, 'login', e);
   }
+};
 
-  const user = await prisma.user.findFirst({ where: { email: loginData.email } });
-
-  if (user == null) {
-    // unknown user
-    req.session.destroy();
-    console.warn(`Login failed - user not found ${loginData.email}`);
-    res
-      .setHeader('Reason', 1)
-      .status(400)
-      .json(getApiResponse({ ok: false, error: 'Invalid email or password' }));
-    return;
-  }
-
-  if (!(await verifyPassword(loginData.password, user.password))) {
-    // invalid password
-    req.session.destroy();
-    res
-      .setHeader('Reason', 2)
-      .status(400)
-      .json(getApiResponse({ ok: false, error: 'Invalid email or password' }));
-    console.warn(`Login failed - wrong password for ${user.email}`);
-    return;
-  }
-
-  const sessionOptions = getIronSessionOptions();
-
-  req.session.user = {
-    id: user?.id,
-    admin: user?.isAdmin ?? false,
-  };
-  await req.session.save();
-  console.log(`User ${user.name} logged in`);
-
-  res.send(
-    getApiResponseWithData<LoginResponseDataType>({
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      sessionExpiresOn: new Date().getTime() + sessionOptions.cookieOptions.ttl * 1000,
-      sessionExpiresInSeconds: sessionOptions.cookieOptions.ttl * 1000,
-    }),
-  );
-}, getIronSessionOptions());
+export default withIronSessionApiRoute(handler, getIronSessionOptions());
