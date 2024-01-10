453751a6cade061a30ab304a4ab1d8a10c8c7994
attila bodi <urostora@gmail.com>
2024-01-10 18:32:12 +0100

    feat(browser): browser shows some file metadata if available

--- home-media-viewer/src/components/content/browseContentCard.tsx
+++ home-media-viewer/src/components/content/browseContentCard.tsx
@@ -1,5 +1,5 @@
 import BrowseContentMenu from "./browserContentMenu";
-import { contentSizeToString } from "@/utils/metaUtils";
+import { MetaType, contentSizeToString, durationInSecToString } from "@/utils/metaUtils";
 import { isVideoByExtension } from '@/utils/frontend/contentUtils'
 
 import type { BrowseResultFile } from "@/types/api/browseTypes"
@@ -9,14 +9,16 @@ import hmvStyle from '@/styles/hmv.module.scss';
 interface BrowseContentCardProps {
     content: BrowseResultFile,
     contentSelected?: (content: BrowseResultFile) => void,
+    contentChanged?: (id: string) => void,
 }
 
 const BrowseContentCard = (props: BrowseContentCardProps): JSX.Element => {
-    const { content, contentSelected } = props;
+    const { content, contentSelected, contentChanged } = props;
 
     const name = content.name;
     const path = content.path;
-    const details = content.isDirectory ? '' : contentSizeToString(content.size ?? 0);
+
+    const metadataTextList: string[] = [];
 
     const isDirectory = content?.isDirectory;
     const isVideo = typeof content?.storedFile?.extension === 'string'
@@ -46,8 +48,42 @@ const BrowseContentCard = (props: BrowseContentCardProps): JSX.Element => {
         ? undefined
         : onContentClickedHandler;
 
-    return (<div className={hmvStyle.browseContentCard}>
-        <div className={hmvStyle.name} title={path} onClick={onContentClickedHandler}>{name}</div>
+    const tooltipText = content.path;
+
+    if (!content.isDirectory) {
+        if (content.storedFile !== null) {
+            const fpsMeta = content.storedFile.metas.find(m => m.metaKey === MetaType.Fps);
+            if (fpsMeta !== undefined) {
+                metadataTextList.push(`${fpsMeta.intValue}fps`);
+            }
+
+            const durationMeta = content.storedFile.metas.find(m => m.metaKey === MetaType.Duration);
+            // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+            if (durationMeta !== undefined && durationMeta?.floatValue !== null) {
+                metadataTextList.push(durationInSecToString(durationMeta.floatValue));
+            }
+
+            const widthMeta = content.storedFile.metas.find(m => m.metaKey === MetaType.ResolutionX);
+            const heightMeta = content.storedFile.metas.find(m => m.metaKey === MetaType.ResolutionY);
+            if (widthMeta !== undefined && heightMeta !== undefined) {
+                metadataTextList.push(`${widthMeta.intValue} x ${heightMeta.intValue}`);
+            }
+        }
+
+        metadataTextList.push(contentSizeToString(content.size ?? 0));
+    }
+
+    const isDisabled = content?.storedFile?.status === 'Disabled';
+    if (isDisabled) {
+        metadataTextList.push('Disabled');
+    }
+
+    const metadataElements = metadataTextList.map((txt, index) => <div key={index}>{txt}</div>);
+
+    return (<div className={`${hmvStyle.browseContentCard} ${isDisabled ? hmvStyle.disabled : ''}`}>
+        <div className={hmvStyle.name} title={path} onClick={onContentClickedHandler}>
+            <abbr title={tooltipText}>{name}</abbr>
+        </div>
         <div
             className={`${hmvStyle.thumbnail} ${hasThumbnailStyle}`}
             style={thumbnailStyle}
@@ -56,8 +92,10 @@ const BrowseContentCard = (props: BrowseContentCardProps): JSX.Element => {
             {videoIcon}
         </div>
         <div className={hmvStyle.details}>
-            <span>{details}</span>
-            <BrowseContentMenu content={content} />
+            <div className={hmvStyle.metadataContainer}>
+                {metadataElements}
+            </div>
+            <BrowseContentMenu content={content} onFileChanged={contentChanged} />
         </div>
     </div>);
 }
