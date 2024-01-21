d29a300f73e5230b26abe03a3b8bc2506ff353a4
attila bodi <urostora@gmail.com>
2024-01-21 13:02:52 +0100

    test(main page): add main menu tests

--- home-media-viewer/__tests__/web/pages/main/mainPageElements.test.ts
+++ home-media-viewer/__tests__/web/pages/main/mainPageElements.test.ts
@@ -7,16 +7,16 @@ import type { Page, ElementHandle } from 'puppeteer';
 import { getPage } from '../helpers/webHelper.helper';
 
 describe('web/pages/main/elements', () => {
-  let page: Page;
+  let desktopPage: Page;
   let mobilePage: Page;
 
   beforeAll(async () => {
-    page = await getPage();
+    desktopPage = await getPage();
     mobilePage = await getPage({ desktop: false });
   });
 
   it('should have user area with name and logout button', async () => {
-    const userHeader = await page.waitForSelector('div[class*="userHeaderContainer"]');
+    const userHeader = await desktopPage.waitForSelector('div[class*="userHeaderContainer"]');
 
     const userNameSpan = await userHeader?.waitForSelector('span[class*="userName"]');
     const logoutButton = await userHeader?.waitForSelector('button');
@@ -30,7 +30,7 @@ describe('web/pages/main/elements', () => {
     let titleLinkMobile: ElementHandle<HTMLAnchorElement> | undefined;
 
     beforeAll(async () => {
-      const titleHeader = await page.waitForSelector('div[class*="titleArea"]');
+      const titleHeader = await desktopPage.waitForSelector('div[class*="titleArea"]');
 
       const titleLinks = await titleHeader?.$$('a[class*="title"]');
       expect(titleLinks?.length).toBe(2);
@@ -54,6 +54,58 @@ describe('web/pages/main/elements', () => {
 
     it('should navigate to index page', async () => {
       expect(await titleLink?.evaluate((l) => l.href)).toBe(`${process.env.APP_URL}/`);
+    });
+  });
+
+  describe('desktop menu', () => {
+    let menuContainer: ElementHandle<HTMLDivElement> | null;
+    let menuToggle: ElementHandle<HTMLDivElement> | null;
+    let menuElementsContainer: ElementHandle<HTMLDivElement> | null;
+
+    beforeAll(async () => {
+      menuContainer = await desktopPage.waitForSelector('div[class*="hmv_mainMenu"]', { timeout: 5000 });
+
+      expect(menuContainer).not.toBeUndefined();
+      expect(menuContainer).not.toBeNull();
+
+      if (menuContainer === null) {
+        return;
+      }
+
+      menuToggle = await menuContainer.waitForSelector('div[class*="hmv_mainMenuToggle"]', { timeout: 5000 });
+      menuElementsContainer = await menuContainer.waitForSelector('div[class*="hmv_mainMenuElements"]', {
+        timeout: 1000,
+      });
+
+      expect(menuToggle).not.toBeUndefined();
+      expect(menuToggle).not.toBeNull();
+
+      expect(menuElementsContainer).not.toBeUndefined();
+      expect(menuElementsContainer).not.toBeNull();
+    });
+
+    it('main menu toggle should not be visible', async () => {
+      expect(await menuToggle?.isVisible()).toBe(false);
+    });
+
+    it('main menu elements should be visible', async () => {
+      expect(await menuElementsContainer?.isVisible()).toBe(true);
+    });
+
+    it('main menu elements should be visible', async () => {
+      const menuElements = await menuContainer?.$$('div[class*="hmv_linkItem"]');
+
+      expect(menuElements).not.toBeUndefined();
+
+      if (menuElements === undefined) {
+        return;
+      }
+
+      expect(menuElements?.length).toBeGreaterThan(0);
+
+      for (const menuElement of menuElements) {
+        expect(await menuElement.isVisible()).toBeTruthy();
+      }
     });
   });
 
@@ -89,8 +141,64 @@ describe('web/pages/main/elements', () => {
     });
   });
 
+  describe('mobile menu', () => {
+    let menuContainer: ElementHandle<HTMLDivElement> | null;
+    let menuToggle: ElementHandle<HTMLDivElement> | null;
+    let menuElementsContainer: ElementHandle<HTMLDivElement> | null;
+
+    beforeAll(async () => {
+      menuContainer = await mobilePage.waitForSelector('div[class*="hmv_mainMenu"]', { timeout: 5000 });
+
+      expect(menuContainer).not.toBeUndefined();
+      expect(menuContainer).not.toBeNull();
+
+      if (menuContainer === null) {
+        return;
+      }
+
+      menuToggle = await menuContainer.waitForSelector('div[class*="hmv_mainMenuToggle"]', { timeout: 5000 });
+      menuElementsContainer = await menuContainer.waitForSelector('div[class*="hmv_mainMenuElements"]', {
+        timeout: 1000,
+      });
+
+      expect(menuToggle).not.toBeUndefined();
+      expect(menuToggle).not.toBeNull();
+
+      expect(menuElementsContainer).not.toBeUndefined();
+      expect(menuElementsContainer).not.toBeNull();
+    });
+
+    it('main menu toggle should be visible', async () => {
+      expect(await menuToggle?.isVisible()).toBe(true);
+    });
+
+    it('main menu elements should not be visible', async () => {
+      expect(await menuElementsContainer?.isVisible()).toBe(false);
+    });
+
+    it('main menu elements should be visible after click to menu toggle', async () => {
+      await menuToggle?.click();
+
+      expect(await menuElementsContainer?.isVisible()).toBe(true);
+
+      const menuElements = await menuContainer?.$$('div[class*="hmv_linkItem"]');
+
+      expect(menuElements).not.toBeUndefined();
+
+      if (menuElements === undefined) {
+        return;
+      }
+
+      expect(menuElements?.length).toBeGreaterThan(0);
+
+      for (const menuElement of menuElements) {
+        expect(await menuElement.isVisible()).toBeTruthy();
+      }
+    });
+  });
+
   afterAll(async () => {
-    await page?.close();
+    await desktopPage?.close();
     await mobilePage?.close();
   });
 });
